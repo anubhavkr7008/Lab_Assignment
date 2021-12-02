6adbc1e6d2730a6b074240053ad22f1957103bd8
Anubhav kumar <59333347+anubhavkr7008@users.noreply.github.com>
2021-12-02 23:13:51 +0530

    Lab 2
    Submitting Lab assignment 2

---
+++ Lab_1/lab2_Q1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1_Q_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the radius : ");
+            double r =Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * r * r ;
+            Console.WriteLine("Area of Circle : " + area);
+            Console.ReadKey();
+        }
+    }
+}
