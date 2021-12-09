4c472099c3f3f4da6fb6d69fb5e83d12dd82318f
Anubhav kumar <59333347+anubhavkr7008@users.noreply.github.com>
2021-12-10 00:22:21 +0530

    Lab assignment 7
    Submitting Lab assignment 7

---
+++ lab 7/A7_Q1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7_Q1
+{
+    public struct student
+    {
+        private int _roll;
+        private string _name;
+        private string _gen;
+        private double _mob;
+
+        public string Name 
+        {
+            get { return _name; }
+            set { _name = value; } 
+        }
+
+        public int Roll
+        {
+            get { return this._roll; }
+            set { this._roll = value; }
+        }
+
+        public string Gender
+        {
+            get { return this._gen; }
+            set { this._gen = value; }
+        }
+
+        public double Mob
+        {
+            get { return this._mob; }
+            set { this._mob = value; }
+        }
+
+        public student (int roll, string name, string gen, int mob)
+        {
+            this._roll= roll;
+            this._name = name;
+            this._gen= gen;
+            this._mob= mob;
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine("\nDetail of studet\n");
+            Console.WriteLine(".....................................\n");
+            Console.WriteLine($"Roll No    : {this._roll}");
+            Console.WriteLine($"Name       : {this._name}");
+            Console.WriteLine($"Gender     : {this._gen}");
+            Console.WriteLine($"Mobile No  : {this._mob}");
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            student s=new student();
+            Console.Write("Enter the Roll no : ");
+            s.Roll = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Name : ");
+            s.Name = (Console.ReadLine());
+            Console.Write("Enter the Gender : ");
+            s.Gender = (Console.ReadLine());
+            Console.Write("Enter the Mobile no : ");
+            s.Mob = Convert.ToDouble(Console.ReadLine());
+            s.printDetails();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab 7/A7_Q2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7_Q2
+{
+    class Program
+    {
+        enum  city { Ranchi =834002 , Hazaribag =825301 , Bokaro =827013 , Bangalore =560002 , Delhi =110002 }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("City Name.\n");
+            string[] cityName = Enum.GetNames(typeof(city));
+            foreach (string i in cityName)
+                Console.WriteLine(i);
+
+            Console.WriteLine("\nPin Code of City\n");
+            int[] n = (int[])Enum.GetValues(typeof(city));
+            foreach (int x in n)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
