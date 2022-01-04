a57a92245e54e953eb412ca3ba4886b5411889b4
Anubhav kumar <59333347+anubhavkr7008@users.noreply.github.com>
2022-01-04 16:15:58 +0530

    Add files via upload
    submitting lab assignment 8

---
+++ lab 8/question_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{
+			Pen pen = new Pen();
+			pen.StartWriting();
+			pen.StopWriting();
+
+			Console.ReadLine();
+		}
+	}
+}
+
+sealed class Pen
+{
+	public void StartWriting()
+	{
+		Console.WriteLine("Start writing");
+	}
+	public void StopWriting()
+	{
+		Console.WriteLine("Stop writing");
+	}
+}
